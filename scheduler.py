a46c186c244073e181401edbb44325c65c20b80a
Jacob Kim <jpoly1219@gmail.com>
2021-05-16 08:14:10 +0000

    fixed a bug where fillBlank will add a person and result in an undesirable shift

--- scheduler.py
+++ scheduler.py
@@ -11,10 +11,12 @@ calendar = [
 sundayNight = input("Who was the night shift for Sunday? ")
 
 class Person:
-    def __init__(self, name, logicalCalendar, hoursWorked, numEvening, numNight):
+    def __init__(self, name, logicalCalendar, hoursWorked, numMorning, numAfternoon, numEvening, numNight):
         self.name = name
         self.logicalCalendar = logicalCalendar
         self.hoursWorked = hoursWorked
+        self.numMorning = numMorning
+        self.numAfternoon = numAfternoon
         self.numEvening = numEvening
         self.numNight = numNight
 
@@ -36,13 +38,18 @@ class Person:
 
         boolPass = True
         while boolPass == True:
-            if (self.logicalCalendar[timeslot][day]==1) or (timeslot==2 and self.numEvening==2) or (timeslot==3 and self.numNight==2):
+            if  (self.logicalCalendar[timeslot][day]==1) or \
+                (timeslot==0 and self.numMorning==2) or \
+                (timeslot==1 and self.numAfternoon==2) or \
+                (timeslot==2 and self.numEvening==2) or \
+                (timeslot==3 and self.numNight==2):
                 timeslot = random.randint(0, 3)
                 day = random.randint(0, 6)
             else:
                 boolPass = False
         # morning shift
         if timeslot == 0:
+            self.numMorning += 1
             if day > 0:
                 self.logicalCalendar[3][day-1] = 1 # last night
             self.logicalCalendar[timeslot][day] = 1 # the shift
@@ -51,6 +58,7 @@ class Person:
             self.logicalCalendar[timeslot+2][day] = 1 # evening
         # afternoon shift
         elif timeslot == 1:
+            self.numAfternoon += 1
             if day > 0:
                 self.logicalCalendar[3][day-1] = 1 # last night
             self.logicalCalendar[timeslot][day] = 1 # the shift
@@ -82,21 +90,39 @@ class Person:
             self.logicalCalendar[timeslot-1][day] = 1 # evening shift
 
 
-def shiftChecker(personList):
-    print("checking shifts...")
+def fillBlank(personList):
+    # print("checking shifts...")
     # if a person's logicalCalendar value == 0 and calendar value == ㅡㅡㅡ
     # then assign that person there
     random.shuffle(personList)
-    for person in personList:
-        done = False
-        for i in range(0, 4):
-            for j in range(0, 7):
-                if person.logicalCalendar[i][j] == 0 and calendar[i][j] == "ㅡㅡㅡ":
-                    calendar[i][j] = person.name
-                    done = True
-                    break
-            if done == True:
-                break # this allows me to break out of exactly two loops
+    for i in range(0, 4):
+        for j in range(0, 7):
+            if calendar[i][j] == "ㅡㅡㅡ":
+                for person in personList:
+                    print("checking case for " + person.name + " at "+ str(i) + ", " + str(j))
+                    if person.logicalCalendar[i][j] == 0:
+                        if i == 0 and person.numMorning < 2:
+                            calendar[i][j] = person.name
+                            person.numMorning += 1
+                            break
+                        if i == 1 and person.numAfternoon < 2:
+                            calendar[i][j] = person.name
+                            person.numAfternoon += 1
+                            break
+                        if i == 2 and person.numEvening < 2:
+                            calendar[i][j] = person.name
+                            person.numEvening += 1
+                            break
+                        if i == 3 and person.numNight < 2:
+                            calendar[i][j] = person.name
+                            person.numNight += 1
+                            break
+                        print(person.name+" already has too many shifts for that timeslot")
+                        continue
+                    else:
+                        print("logCal of "+person.name+" has no space")
+                        continue
+
                     
 
 logCalMingeunkim = [
@@ -129,11 +155,11 @@ logCalWootaekhong = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0]
 ]
-mingeunkim = Person("김민근", logCalMingeunkim, 0, 0, 0)
-yongheekwon = Person("권용희", logCalYongheekwon, 0, 0, 0)
-junghoonban = Person("반정훈", logCalJunghoonban, 0, 0, 0)
-junehyungkim = Person("김준형", logCalJunehyungkim, 0, 0, 0)
-wootaekhong = Person("홍우택", logCalWootaekhong, 0, 0, 0)
+mingeunkim = Person("김민근", logCalMingeunkim, 0, 0, 0, 0, 0)
+yongheekwon = Person("권용희", logCalYongheekwon, 0, 0, 0, 0, 0)
+junghoonban = Person("반정훈", logCalJunghoonban, 0, 0, 0, 0, 0)
+junehyungkim = Person("김준형", logCalJunehyungkim, 0, 0, 0, 0, 0)
+wootaekhong = Person("홍우택", logCalWootaekhong, 0, 0, 0, 0, 0)
 
 personList = [mingeunkim, yongheekwon, junghoonban, junehyungkim, wootaekhong]
 
@@ -147,8 +173,16 @@ print("pre-check:")
 for i in range(0, 4):
     print(calendar[i])
 
-for i in range(0, 3):
-    shiftChecker(personList)
+
+# run fillBlank until no more ㅡㅡㅡ exists
+"""
+for i in range(0, 4):
+    print(i)
+    while "ㅡㅡㅡ" in calendar[i]:
+        fillBlank(personList)
+        print("blank "+str(i)+" filled")
+"""
+fillBlank(personList)
 
 print("final:")
 for i in range(0, 4):
--- test.py
+++
@@ -1,6 +0,0 @@
-import random
-
-
-for i in range (0, 100):
-    x = random.randint(0, 4)
-    print(x)
